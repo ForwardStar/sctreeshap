6b3fe2938921cabbc0748a01a1d5d297841ad1e9
ForwardStar <119010351@link.cuhk.edu.cn>
2021-07-19 20:55:31 +0800

    Configuration updated

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
   name="sctreeshap",
-  version="0.1.0",
+  version="0.1.1",
   author="Haoxuan Xie",
   author_email="haoxuanxie@link.cuhk.edu.cn",
   description="sctreeshap: a cluster tree data structure, and for shap analysis",
@@ -17,19 +17,10 @@ setuptools.setup(
   long_description_content_type="text/markdown",
   license="LICENSE",
   url="https://github.com/ForwardStar/sctreeshap",
-  packages=setuptools.find_packages(where='sctreeshap'),
-  package_dir={'': 'sctreeshap'},
-  py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob.glob('sctreeshap/*.py')],
+  packages=setuptools.find_packages(),
   classifiers=[
   "Programming Language :: Python :: 3.5",
   "License :: OSI Approved :: MIT License",
   "Operating System :: OS Independent",
-  ],
-  install_requires=['shap>=0.37.0',
-                    'matplotlib>=3.3.2',
-                    'anndata>=0.7.6',
-                    'numpy>=1.19.2',
-                    'pandas>=0.25.2',
-                    'scikit-learn>=0.23.1',
-                    'xgboost>=1.3.3']
+  ]
 )
